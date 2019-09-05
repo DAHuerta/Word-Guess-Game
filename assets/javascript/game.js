dc0c6c353609147924c579f141e007373a50bb1e
Daniel Huerta <danielhuerta@Daniels-MacBook-Pro.local>
2019-09-05 17:24:02 -0600

    update horror movie list

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,4 +5,6 @@
 // create letters guessed and tries remaining variable
 
 // create if and else rules of the game
+var horrorMovies = ["It", "It Follows", "Martyrs", "Evil Dead", "Hereditary", "Trick r Treat", "Sinister", "The Conjuring", "Reanimator", "Mothman Prophecies", "Get Out", "Us", "The Thing", "The Shining", "Butterfly Effect", "The House of 1000 Corpses", "The Devils Rejects", "Popcorn", "Nightmare on Elmstreet", "Halloween", "Friday the 13th", "Poultrygeist", "Mothers Day", "The Decent", "Seven", "Pans Labyrinth", "Audition", "Shutter", "The Ring", "The Babadook", "The Exorcist", "Cabin in the Woods", "Texas Chainsaw Massacre", "Gacy", "Behind the Mask", "Alien", "Jaws", "Chuckies", "Leprechaun", "Psycho", "Midsommer", "Insidious", "A Quiet Place", "The Cabin in the Woods", "Cabin Fever", "Saw", "Carrie", "The Strangers", "Silence of the Lambs", "Would You Rather", "Your Next", "Hatchet", "Inside", "Splinter", "The Fog"
+];
 
