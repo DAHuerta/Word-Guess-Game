62e45f21e531c493604c25c3c63fb1e568a1c838
Daniel Huerta <danielhuerta@Daniels-MBP.PK5001Z>
2019-09-05 19:46:16 -0600

    adding game functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,42 @@
+var horrorMovies = ["it", "it follows", "martyrs", "evil dead", "hereditary", "trick r treat", "sinister", "the conjuring", "reanimator", "mothman prophecies", "get out", "us", "the thing", "the shining", "butterfly effect", "the house of 1000 corpses", "the devils rejects", "popcorn", "nightmare on elmstreet", "halloween", "friday the 13th", "poultrygeist", "mothers day", "the decent", "seven", "pans labyrinth", "audition", "shutter", "the ring", "the babadook", "the exorcist", "cabin in the woods", "texas chainsaw massacre", "gacy", "behind the mask", "alien", "jaws", "chuckies", "leprechaun", "psycho", "midsommer", "insidious", "a quiet place", "the cabin in the woods", "cabin fever", "saw", "carrie", "the strangers", "silence of the lambs", "would you rather", "your next", "hatchet", "inside", "splinter", "the fog"
+];
+
+var winCount = 0;
+var guessesRemainingCount = 8;
+
+var wins = document.getElementById("wins");
+var currentWord = document.getElementById("currentWord");
+var guessesRemaining = document.getElementById("guessesRemaining");
+var lettersGuessed = document.getElementById("lettersGuessed");
+
+
+var selectedWord = [];
+var lettersGuessed =[];
+
+document.onkeyup = function(event) {
+
+    var userGuess = event.key;
+    
+    var word = horrorMovies[Math.floor(Math.random() * horrorMovies.length)];
+    
+    for (var i = 0; i < word.length; i++) {
+        selectedWord[i]= " _ "; 
+    }
+
+    if (guessesRemainingCount > 0) {
+        lettersGuessed.push(userGuess);
+    }
+
+    
+    
+
+    wins.textContent = "Wins: " + winCount;
+    currentWord.textContent = "Current Word: " + selectedWord;
+    guessesRemaining.textContent = "Number of guesses remaining: " + guessesRemainingCount;
+    lettersGuessed.textContent = "Letters Guessed: " + lettersGuessed;
+    
+    
+}
 // create an array of horror movies
 
 // create wins and losses variable
@@ -5,6 +44,4 @@
 // create letters guessed and tries remaining variable
 
 // create if and else rules of the game
-var horrorMovies = ["It", "It Follows", "Martyrs", "Evil Dead", "Hereditary", "Trick r Treat", "Sinister", "The Conjuring", "Reanimator", "Mothman Prophecies", "Get Out", "Us", "The Thing", "The Shining", "Butterfly Effect", "The House of 1000 Corpses", "The Devils Rejects", "Popcorn", "Nightmare on Elmstreet", "Halloween", "Friday the 13th", "Poultrygeist", "Mothers Day", "The Decent", "Seven", "Pans Labyrinth", "Audition", "Shutter", "The Ring", "The Babadook", "The Exorcist", "Cabin in the Woods", "Texas Chainsaw Massacre", "Gacy", "Behind the Mask", "Alien", "Jaws", "Chuckies", "Leprechaun", "Psycho", "Midsommer", "Insidious", "A Quiet Place", "The Cabin in the Woods", "Cabin Fever", "Saw", "Carrie", "The Strangers", "Silence of the Lambs", "Would You Rather", "Your Next", "Hatchet", "Inside", "Splinter", "The Fog"
-];
 
