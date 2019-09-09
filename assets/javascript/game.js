206a9c0bec163cfd556057bd584bd1d31e895e6f
Daniel Huerta <danielhuerta@Daniels-MacBook-Pro.local>
2019-09-09 10:00:57 -0600

    fix game functionality, one bug left to fix

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-var horrorMovies = ["it", "itfollows", "martyrs", "evildead", "hereditary", "trickrtreat", "sinister", "theconjuring", "reanimator", "mothmanprophecies", "getout", "us", "thething", "theshining", "butterflyeffect", "thehouseof1000corpses", "thedevilsrejects", "popcorn", "nightmareonelmstreet", "halloween", "fridaythe13th", "poultrygeist", "mothersday", "thedecent", "seven", "panslabyrinth", "audition", "shutter", "thering", "thebabadook", "theexorcist", "cabininthewoods", "texaschainsaw massacre", "gacy", "behindthemask", "alien", "jaws", "chuckies", "leprechaun", "psycho", "midsommer", "insidious", "aquietplace", "thecabininthewoods", "cabinfever", "saw", "carrie", "thestrangers", "silenceofthelambs", "wouldyourather", "yournext", "hatchet", "inside", "splinter", "thefog", "jeeperscreepers"
+var horrorMovies = ["it", "itfollows", "martyrs", "evildead", "hereditary", "trickrtreat", "sinister", "theconjuring", "reanimator", "mothmanprophecies", "getout", "us", "thething", "theshining", "butterflyeffect", "thehouseof1000corpses", "thedevilsrejects", "popcorn", "nightmareonelmstreet", "halloween", "fridaythe13th", "poultrygeist", "mothersday", "thedecent", "seven", "panslabyrinth", "audition", "shutter", "thering", "thebabadook", "theexorcist", "cabininthewoods", "texaschainsaw massacre", "gacy", "behindthemask", "alien", "jaws", "chuckie", "leprechaun", "psycho", "midsommer", "insidious", "aquietplace", "thecabininthewoods", "cabinfever", "saw", "carrie", "thestrangers", "silenceofthelambs", "wouldyourather", "yournext", "hatchet", "inside", "splinter", "thefog", "jeeperscreepers"
 ];
 
 var winCount = 0;
@@ -8,10 +8,12 @@ var wins = document.getElementById("wins");
 var currentWord = document.getElementById("currentWord");
 var guessesRemaining = document.getElementById("guessesRemaining");
 var lettersGuessed = document.getElementById("lettersGuessed");
+var word = ""
 
 
 var selectedWord = [];
 var lettersGuessedCount =[];
+var correctKeys = []
 
 //     function correctLetter(x) {
 //         var correctKeys = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
@@ -19,50 +21,76 @@ var lettersGuessedCount =[];
 //         console.log("Begin Game!");
 //     } 
 // }
-
-
-//   document.onkeyup = function(event) {
-
-    var userGuess = event.key;
-    
-    var word = horrorMovies[Math.floor(Math.random() * horrorMovies.length)];
+function resetGame() {
+    // pick a random word
+    word = horrorMovies[Math.floor(Math.random() * horrorMovies.length)];
 
     for (var i = 0; i < word.length; i++) {
-        selectedWord[i]= "_"; 
+        selectedWord[i]= "_";
     }
-console.log(word)
+    // reset letters guessed
+    lettersGuessedCount = []
+    // reset guesses remaining
+    guessesRemainingCount = 9
+
+    currentWord.textContent = "Current Word: " + selectedWord.join("");
+    
+}
+
+function winsLosses() {
+    // check win and losses, if won or loss reset game
+    if (selectedWord.join("") === word) {
+        winCount++;
+        resetGame();
+    } else {
+
+    if (guessesRemainingCount === 0) {
+    alert("YOU LOSE!");
+    resetGame()
+    }
+            
+    }
+}
+
+    resetGame()
+
+  document.onkeyup = function(event) {
+
+    var userGuess = event.key;
+
+    console.log(word)
     var remainingLetters = word.length;
 
-    for (var j = 0; j < word.length; j++) {
+    if (remainingLetters > 0) {
+        console.log(remainingLetters);
         
-    if (word[j] === userGuess) {
-        selectedWord[j] = userGuess;
-        remainingLetters--;
+        
+        for (var j = 0; j < word.length; j++) {
+            
+            if (word[j] === userGuess) {
+                selectedWord[j] = userGuess;
+            }
+        }
+        
+        lettersGuessedCount.push(userGuess);
+        
+        console.log(lettersGuessedCount)
+        
+        
     }
-    }
-    
-    lettersGuessedCount.push(userGuess);
-    
-    console.log(lettersGuessedCount);
-    
-    if (userGuess === selectedWord) {
-        winCount++;
-    } else {
+
+    if (word.indexOf(userGuess) === -1) {
         guessesRemainingCount--;
     }
-
-    if (guessesRemainingCount <= 0) {
-        alert("YOU LOSE!");
-    } 
-       
-
-    wins.textContent = "Wins: " + winCount;
-    currentWord.textContent = "Current Word: " + selectedWord.join(" ");
-    guessesRemaining.textContent = "Number of guesses remaining: " + guessesRemainingCount;
-    lettersGuessed.textContent = "Letters Guessed: " + lettersGuessedCount;
+   
+    winsLosses()
+        
+        wins.textContent = "Wins: " + winCount;
+        currentWord.textContent = "Current Word: " + selectedWord.join("");
+        guessesRemaining.textContent = "Number of guesses remaining: " + guessesRemainingCount;
+        lettersGuessed.textContent = "Letters Guessed: " + lettersGuessedCount;
     
-    
-// }
+}
 // create an array of horror movies
 
 // create wins and losses variable
