7f24bad34790a2a62ac7875ba84538e85a77ecdc
Daniel Huerta <danielhuerta@Daniels-MacBook-Pro.local>
2019-09-09 20:36:19 -0600

    css added to game. one bug needs to be fixed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,8 @@
-var horrorMovies = ["it", "itfollows", "martyrs", "evildead", "hereditary", "trickrtreat", "sinister", "theconjuring", "reanimator", "mothmanprophecies", "getout", "us", "thething", "theshining", "butterflyeffect", "thehouseof1000corpses", "thedevilsrejects", "popcorn", "nightmareonelmstreet", "halloween", "fridaythe13th", "poultrygeist", "mothersday", "thedecent", "seven", "panslabyrinth", "audition", "shutter", "thering", "thebabadook", "theexorcist", "cabininthewoods", "texaschainsaw massacre", "gacy", "behindthemask", "alien", "jaws", "chuckie", "leprechaun", "psycho", "midsommer", "insidious", "aquietplace", "thecabininthewoods", "cabinfever", "saw", "carrie", "thestrangers", "silenceofthelambs", "wouldyourather", "yournext", "hatchet", "inside", "splinter", "thefog", "jeeperscreepers"
+var horrorMovies = ["it", "itfollows", "martyrs", "evildead", "hereditary", "trickrtreat", "sinister", "theconjuring", "reanimator", "mothmanprophecies", "getout", "us", "thething", "theshining", "butterflyeffect", "thehouseof1000corpses", "thedevilsrejects", "popcorn", "nightmareonelmstreet", "halloween", "fridaythe13th", "poultrygeist", "mothersday", "thedecent", "seven", "panslabyrinth", "audition", "shutter", "thering", "thebabadook", "theexorcist", "cabininthewoods", "texaschainsawmassacre", "gacy", "behindthemask", "alien", "jaws", "chuckie", "leprechaun", "psycho", "midsommer", "insidious", "aquietplace", "thecabininthewoods", "cabinfever", "saw", "carrie", "thestrangers", "silenceofthelambs", "wouldyourather", "yournext", "hatchet", "inside", "splinter", "thefog", "jeeperscreepers"
 ];
 
 var winCount = 0;
-var guessesRemainingCount = 9;
+var guessesRemainingCount = 10;
 
 var wins = document.getElementById("wins");
 var currentWord = document.getElementById("currentWord");
@@ -25,13 +25,11 @@ function resetGame() {
     // pick a random word
     word = horrorMovies[Math.floor(Math.random() * horrorMovies.length)];
 
-    for (var i = 0; i < word.length; i++) {
-        selectedWord[i]= "_";
-    }
+    selectedWord = "_".repeat(word.length).split("");
     // reset letters guessed
-    lettersGuessedCount = []
+    lettersGuessedCount = [];
     // reset guesses remaining
-    guessesRemainingCount = 9
+    guessesRemainingCount = 10;
 
     currentWord.textContent = "Current Word: " + selectedWord.join("");
     
@@ -45,8 +43,8 @@ function winsLosses() {
     } else {
 
     if (guessesRemainingCount === 0) {
-    alert("YOU LOSE!");
-    resetGame()
+        alert("YOU LOSE!");
+        resetGame();
     }
             
     }
@@ -75,8 +73,7 @@ function winsLosses() {
         lettersGuessedCount.push(userGuess);
         
         console.log(lettersGuessedCount)
-        
-        
+            
     }
 
     if (word.indexOf(userGuess) === -1) {
